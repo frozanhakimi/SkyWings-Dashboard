52215562bb9e83d234fb49b1ebb57ef1b6ef352a
Asmasultani <asmasultani587@gmail.com>
2026-09-11 05:05:53 -0700

    Add some changes to js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 const loginBtn = document.querySelector(".login-btn");
 
 loginBtn.addEventListener("click", function () {
-    window.location.href = "login.html";
+    window.location.href = "index.html";
 });
