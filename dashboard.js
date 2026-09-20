94a8e27e5220cc82ac759930e4b4d4a4ae78eeab
Frozan Hakimi <hakimifrozan0@gmail.com>
2026-09-20 10:58:36 +0430

    Change js login path

--- dashboard.js
+++ dashboard.js
@@ -1,17 +1,13 @@
-
 /* =========================================
-   LOGIN BUTTON -========================== */
-   document.addEventListener('DOMContentLoaded', function () {
+   LOGIN BUTTON
+========================================= */
+
+document.addEventListener('DOMContentLoaded', function () {
     const loginBtns = document.querySelectorAll('.login-btn');
-    
+
     loginBtns.forEach(btn => {
         btn.addEventListener('click', function () {
             window.location.href = "login-page/index.html";
         });
     });
-
-const loginBtn = document.querySelector(".login-btn");
-
-loginBtn.addEventListener("click", function () {
-    window.location.href = "login-page/index.html";
 });
